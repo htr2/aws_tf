67dba166fdbc98607c47bbeee571ad014e440094
htr2 <hevorss@gmail.com>
2022-04-25 15:10:43 +0100

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # aws_tf
 
-creates vpc, subnet, igw, instance with basic web server
+uses creates terraform to create aws
+vpc with subnet, igw, route table 
+ec2 instance with basic web server
+
+# Usage
+terraform apply
+
+# Examples
+
+# Requirements
+requires aws account with programatic access, linux shell (bash with aws cli with and access credentials (env var or ~./aws/credentials) )
+
+# Inputs
+in files 
+
+# Outputs
+
--- vpc.tf
+++ vpc.tf
@@ -77,20 +77,3 @@ resource "aws_security_group" "public_security_group" {
 }
 
 
-resource "aws_instance" "vm1" {
-  instance_type = "t2.micro"
-  ami           = "ami-03ededff12e34e59e" #Amazon Linux 2 Kernel 5.10 AMI 2.0.20220406.1 x86_64 HVM gp2
-
-  tags = {
-    Name = "vm1"
-  }
-
-  key_name               = "hvoelksen-aws-mgmt"
-  vpc_security_group_ids = [aws_security_group.public_security_group.id]
-  subnet_id              = aws_subnet.us-east-1a-public_subnet.id
-  user_data              = templatefile("userdata.sh", {})
-
-  root_block_device {
-    volume_size = 8
-  }
-}
