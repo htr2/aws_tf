24101aab3d6a4a2158797abe8ce39c6920dcb65c
htr2 <hevorss@gmail.com>
2022-05-09 12:44:40 +0100

    github workflow

--- initial_setup/iam.tf
+++ initial_setup/iam.tf
@@ -1,7 +1,14 @@
+#data "aws_caller_identity" "current" {}
+#
+#output "account_id" {
+#  value = data.aws_caller_identity.current.account_id
+#}
+
 resource "aws_iam_account_alias" "alias" {
   account_alias = "hv-aws-management"
 }
 
+#create admin user group and user
 resource "aws_iam_user" "user1" {
   name = "Administrator"
   #path = "/system/"
@@ -33,7 +40,15 @@ resource "aws_iam_group" "Administrators" {
   #path = "/users/"
 }
 
+resource "aws_iam_user_group_membership" "user-group" {
+  user = aws_iam_user.user1.name
 
+  groups = [
+    aws_iam_group.Administrators.name,
+  ]
+}
+
+#enforce mfa for admin user group 
 resource "aws_iam_policy" "AdministratorAccess-MFAenforced" {
   name        = "AdministratorAccess-MFAenforced"
   description = "requires MFA..."
@@ -59,21 +74,13 @@ resource "aws_iam_group_policy_attachment" "policy-attach1" {
   policy_arn = aws_iam_policy.AdministratorAccess-MFAenforced.id
 }
 
+##example for secondary policy attachment using aws managed policy ie not a tf name
 #resource "aws_iam_group_policy_attachment" "policy-attach2" {
-#  group      = aws_iam_group.billingadmins.name
+#  group      = aws_iam_group.Administrators.name
 #  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 #}
 
-resource "aws_iam_user_group_membership" "user-group" {
-  user = aws_iam_user.user1.name
-
-  groups = [
-    aws_iam_group.Administrators.name,
-  ]
-}
-
-
-###mfa
+###user mfa
 #left out as one cannot complete activate mfa device (authenticator app) by code....requires QRcode scanning by app or code entry...
 #so if manual required do all manual...
 #also aws provider has bug and mangles qr code output unusable, aws cli works..., string seed might work too...
@@ -100,3 +107,60 @@ output "mfa_qr" {
   value     = aws_iam_virtual_mfa_device.testing_mfa_qr.qr_code_png
 }
 */
+
+
+#setup github OIDC idp
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+#github role trust
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:htr2/aws_*:*", #!! limit the trust of who can call the role to our repo(s)!!! otherwise open to all of github!!!
+        #"repo:org2/*:*"
+      ]
+    }
+  }
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+#github role permission
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    resources = ["arn:aws:iam::${aws_organizations_account.production.account_id}:role/OrganizationAccountAccessRole"]
+  }
+
+
+resource "aws_iam_policy" "github_actions" {
+  name        = "github-actions"
+  description = "Grant Github Actions the ability assume role in prod account"
+  policy      = data.aws_iam_policy_document.github_actions.json
+}
+
+#role 
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
+
--- test.tf
+++ test.tf
@@ -1,9 +1,75 @@
-resource "aws_vpc" "us-east-1_vpc" {
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#
+#resource "aws_vpc" "us-east-1_vpc" {
+#  cidr_block           = "10.10.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#
+#  tags = {
+#    Name = "us-east-1_vpc"
+#  }
+#}
 
-  tags = {
-    Name = "us-east-1_vpc"
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+##setup github OIDC idp
+#resource "aws_iam_openid_connect_provider" "github" {
+#  url             = "https://token.actions.githubusercontent.com"
+#  client_id_list  = ["sts.amazonaws.com"]
+#  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+#}
+
+
+#github role trust
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:htr2/aws_*:*", #!! limit the trust of who can call the role to our repo(s)!!! otherwise open to all of github!!!
+        #"repo:org2/*:*"
+      ]
+    }
   }
+}
+
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+#github role permission
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    resources = ["arn:aws:iam::${aws_organizations_account.production.account_id}:role/OrganizationAccountAccessRole"]
+  }
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name        = "github-actions"
+  description = "Grant Github Actions the ability assume role in prod account"
+  policy      = data.aws_iam_policy_document.github_actions.json
+}
+
+
+#role 
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
 }
